5c1330dc1bdc39e12c2e56c924f91555b60b9e70
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-16 11:29:06 -0700

    Add prototype for total score
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 let roundScoreArray = [];
-
+let totalScore = 0;
+//object to make players and a way to track score
 function Players(name, roundScore, totalScore) {
   this.playerName = name;
   this.roundScore = roundScore;
   this.totalScore = totalScore;
 }
-
+//random number generator
 let rollDice = function () {
   diceRoll = 1 + Math.floor(Math.random() * 6);
   return diceRoll;
 };
-
+//if 1 sets array length to zero
 Players.prototype.createRoundScore = function (diceRoll) {
   if (diceRoll === 1) {
     this.roundScore = 0;
@@ -20,5 +21,12 @@ Players.prototype.createRoundScore = function (diceRoll) {
   return rollDice;
 };
 
-let player1 = new Players("Ben", roundScoreArray, "totalScore")
+// prototype to set total score value
+Players.prototype.createTotalScore = function (total) {
+  return this.totalScore = this.totalScore + total;
+}
+
+//creates player to send score into array inside player
+let player1 = new Players("Ben", roundScoreArray, totalScore)
 player1.createRoundScore(6);
+player1.createTotalScore(5);
